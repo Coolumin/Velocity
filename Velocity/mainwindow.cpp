03b62330a3841e98d195aca84ee90dcc4dac61cf
Pandoriaantje <pandoriaantje@gmail.com>
2024-10-15 22:47:48 +0200

    Updated to use path() for qurl filepath

--- Velocity/mainwindow.cpp
+++ Velocity/mainwindow.cpp
@@ -376,7 +376,13 @@ void MainWindow::LoadFiles(QList<QUrl> &filePaths)
 {
     for (int i = 0; i < filePaths.size(); i++)
     {
-        std::string fileName = filePaths.at(i).toLocalFile().toStdString();
+        std::string fileName;
+#ifdef __WIN32__
+        fileName = QString(filePaths.at(i).path()).mid(1).toStdString();
+#else
+        fileName = QString(filePaths.at(i).path()).replace("file://", "").toStdString();
+#endif
+
 
         // make sure the file exists
         if (!QFile::exists(QString::fromStdString(fileName)))
